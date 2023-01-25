1251a05647937816e28a31d6e38184e5a2b1a731
saraslemos <saradslemos@gmail.com>
2023-01-25 20:22:14 -0300

    Meus projetos

---
+++ crud-spring/src/main/java/com/loiane/controller/CourseController.java
@@ -0,0 +1,73 @@
+package com.loiane.controller;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import com.loiane.model.Course;
+import com.loiane.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@Validated
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor 
+public class CourseController {
+    
+    private CourseRepository courseRepository;
+
+    /**
+     * @return
+     */
+    @GetMapping
+    public @ResponseBody List<Course> list() {
+        return courseRepository.findAll();  
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody @Valid Course course) {
+
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+        //return ResponseEntity.status(HttpStatus.CREATED)
+        //.body(courseRepository.save(course));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody @Valid Course course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
